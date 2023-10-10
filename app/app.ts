028941c4919fdde050308f296fbf8c2761ae256a
cryptonft <66502208+katexu7@users.noreply.github.com>
2023-10-11 09:00:44 +1100

    xero

--- app/app.ts
+++ app/app.ts
@@ -1,9 +1,10 @@
 require('dotenv').config();
 import express from 'express';
+const cors = require('cors');
 import { Request, Response } from 'express';
 import jwtDecode from 'jwt-decode';
 import { TokenSet } from 'openid-client';
-import { XeroAccessToken, XeroIdToken, XeroClient, Contact, LineItem, Invoice, Invoices, Phone, Contacts } from 'xero-node';
+import { XeroAccessToken, XeroIdToken, XeroClient } from 'xero-node';
 
 const session = require('express-session');
 
@@ -11,6 +12,9 @@ const client_id: string = process.env.CLIENT_ID;
 const client_secret: string = process.env.CLIENT_SECRET;
 const redirectUrl: string = process.env.REDIRECT_URI;
 const scopes: string = 'openid profile email accounting.settings accounting.reports.read accounting.journals.read accounting.contacts accounting.attachments accounting.transactions offline_access';
+//const scopes: string = 'openid profile email offline_access hq.clients.read hq.staff.read'
+
+//const scopes: string = 'openid profile email offline_access practicemanager.read practicemanager.job.read practicemanager.client.read practicemanager.staff.read practicemanager.time.read'
 
 const xero = new XeroClient({
 	clientId: client_id,
@@ -26,9 +30,9 @@ if (!client_id || !client_secret || !redirectUrl) {
 const app: express.Application = express();
 
 app.use(express.static(__dirname + '/build'));
-
+app.use(cors());
 app.use(session({
-	secret: 'something crazy',
+	secret: client_secret,
 	resave: false,
 	saveUninitialized: true,
 	cookie: { secure: false },
@@ -48,11 +52,17 @@ app.get('/', (req: Request, res: Response) => {
 	res.send(`<a href='/connect'>Connect to Xero</a>`);
 });
 
+app.get('/welcome', (req: Request, res: Response) => {
+	res.send(`you're in`);
+});
+
 app.get('/connect', async (req: Request, res: Response) => {
 	try {
+		//console.log("connecting...")
 		const consentUrl: string = await xero.buildConsentUrl();
 		res.redirect(consentUrl);
 	} catch (err) {
+		//console.log("err...")
 		res.send('Sorry, something went wrong');
 	}
 });
@@ -61,7 +71,7 @@ app.get('/callback', async (req: Request, res: Response) => {
 	try {
 		const tokenSet: TokenSet = await xero.apiCallback(req.url);
 		await xero.updateTenants();
-
+		
 		const decodedIdToken: XeroIdToken = jwtDecode(tokenSet.id_token);
 		const decodedAccessToken: XeroAccessToken = jwtDecode(tokenSet.access_token);
 
@@ -76,79 +86,12 @@ app.get('/callback', async (req: Request, res: Response) => {
 
 		console.log(authData);
 
-		res.redirect('/organisation');
+		res.redirect('/welcome');
 	} catch (err) {
 		res.send('Sorry, something went wrong');
 	}
 });
 
-app.get('/organisation', async (req: Request, res: Response) => {
-	try {
-		const tokenSet: TokenSet = await xero.readTokenSet();
-		console.log(tokenSet.expired() ? 'expired' : 'valid');
-		const response: any = await xero.accountingApi.getOrganisations(req.session.activeTenant.tenantId);
-		res.send(`Hello, ${response.body.organisations[0].name}`);
-	} catch (err) {
-		res.send('Sorry, something went wrong');
-	}
-});
-
-app.get('/invoice', async (req: Request, res: Response) => {
-	try {
-		const contacts = await xero.accountingApi.getContacts(req.session.activeTenant.tenantId);
-		console.log('contacts: ', contacts.body.contacts);
-		const where = 'Status=="ACTIVE" AND Type=="SALES"';
-		const accounts = await xero.accountingApi.getAccounts(req.session.activeTenant.tenantId, null, where);
-		console.log('accounts: ', accounts.body.accounts);
-		const contact: Contact = {
-			contactID: contacts.body.contacts[0].contactID
-		};
-		const lineItem: LineItem = {
-			accountID: accounts.body.accounts[0].accountID,
-			description: 'consulting',
-			quantity: 1.0,
-			unitAmount: 10.0
-		};
-		const invoice: Invoice = {
-			lineItems: [lineItem],
-			contact: contact,
-			dueDate: '2021-09-25',
-			date: '2021-09-24',
-			type: Invoice.TypeEnum.ACCREC
-		};
-		const invoices: Invoices = {
-			invoices: [invoice]
-		};
-		const response = await xero.accountingApi.createInvoices(req.session.activeTenant.tenantId, invoices);
-		console.log('invoices: ', response.body.invoices);
-		res.json(response.body.invoices);
-	} catch (err) {
-		res.json(err);
-	}
-});
-
-app.get('/contact', async (req: Request, res: Response) => {
-	try {
-		const contact: Contact = {
-			name: "Bruce Banner",
-			emailAddress: "hulk@avengers.com",
-			phones: [
-				{
-					phoneNumber:'555-555-5555',
-					phoneType: Phone.PhoneTypeEnum.MOBILE
-				}
-			]
-		};
-		const contacts: Contacts = {  
-			contacts: [contact]
-		}; 
-		const response = await xero.accountingApi.createContacts(req.session.activeTenant.tenantId, contacts);
-		console.log('contacts: ', response.body.contacts);
-		res.json(response.body.contacts);
-	} catch (err) {
-		res.json(err);
-	}
-});
 
 const PORT = process.env.PORT || 5000;
 
