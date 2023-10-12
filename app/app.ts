248eae1d0a3bf6b727508b62144525ecca420d44
cryptonft <66502208+katexu7@users.noreply.github.com>
2023-10-13 00:40:12 +1100

    Update app.ts

--- app/app.ts
+++ app/app.ts
@@ -87,9 +87,9 @@ app.get('/callback', async (req: Request, res: Response) => {
 
 		res.redirect('http://localhost:3000');
     } catch (err) {
-		console.log("Callback URL:", req.url);
-		console.error("Error in /callback:", err);
-		res.status(500).send('Sorry, something went wrong: ' + (err.message || JSON.stringify(err)));
+		//console.log("Callback URL:", req.url);
+		//console.error("Error in /callback:", err);
+		res.status(500).send("Callback URL:" + req.url);
 	}
 	
 });
